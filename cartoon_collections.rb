3329ccb7b474101a03b64fe40dbdb94fcb67d048
Richard Sanchez <richard.charles.s@gmail.com>
2018-06-27 19:09:11 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,8 @@ def long_planeteer_calls(array)
   array.any? { |calls|  calls.length >= 4 ? false : true  }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.include?(cheese_types)
 end
